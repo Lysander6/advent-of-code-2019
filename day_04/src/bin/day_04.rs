2c10d0fc669e31cf953ff2ff2661990a837503d8
Lysander <lysander6@gmail.com>
2024-11-21 17:54:28 +0100

    Day 4, part 2 the dumb way ¯\_(ツ)_/¯

--- day_04/src/bin/day_04.rs
+++ day_04/src/bin/day_04.rs
@@ -1,5 +1,6 @@
-use day_04::dumb;
+use day_04::{dumb, dumb_part2};
 
 fn main() {
     println!("Part 1: {}", dumb(172_930, 683_082));
+    println!("Part 2: {}", dumb_part2(172_930, 683_082));
 }
--- day_04/src/lib.rs
+++ day_04/src/lib.rs
@@ -31,6 +31,41 @@ pub fn dumb(min: u32, max: u32) -> usize {
     count
 }
 
+#[must_use]
+pub fn dumb_part2(min: u32, max: u32) -> usize {
+    let mut current = min;
+    let mut count = 0;
+
+    while current <= max {
+        let (a, b, c, d, e, f) = (
+            current / 100_000 % 10,
+            current / 10_000 % 10,
+            current / 1_000 % 10,
+            current / 100 % 10,
+            current / 10 % 10,
+            current % 10,
+        );
+
+        if a <= b
+            && b <= c
+            && c <= d
+            && d <= e
+            && e <= f
+            && ((a == b && b != c)
+                || (a != b && b == c && c != d)
+                || (b != c && c == d && d != e)
+                || (c != d && d == e && e != f)
+                || (d != e && e == f))
+        {
+            count += 1;
+        }
+
+        current += 1;
+    }
+
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,5 +89,10 @@ mod tests {
     #[test]
     fn test_dumb() {
         assert_eq!(dumb(172_930, 683_082), 1675);
+    }
+
+    #[test]
+    fn test_dumb_part2() {
+        assert_eq!(dumb_part2(172_930, 683_082), 1142);
     }
 }
