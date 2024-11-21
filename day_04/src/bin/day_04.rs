b21fa033643a091d1991c6881197f077dd1b09c8
Lysander <lysander6@gmail.com>
2024-11-21 17:46:38 +0100

    Day 4, part 1 the dumb way

---
+++ day_04/src/bin/day_04.rs
@@ -0,0 +1,5 @@
+use day_04::dumb;
+
+fn main() {
+    println!("Part 1: {}", dumb(172_930, 683_082));
+}
---
+++ day_04/src/lib.rs
@@ -0,0 +1,58 @@
+#![allow(clippy::many_single_char_names)]
+
+#[must_use]
+pub fn dumb(min: u32, max: u32) -> usize {
+    let mut current = min;
+    let mut count = 0;
+
+    while current <= max {
+        let (a, b, c, d, e, f) = (
+            current / 100_000 % 10,
+            current / 10_000 % 10,
+            current / 1_000 % 10,
+            current / 100 % 10,
+            current / 10 % 10,
+            current % 10,
+        );
+
+        if a <= b
+            && b <= c
+            && c <= d
+            && d <= e
+            && e <= f
+            && (a == b || b == c || c == d || d == e || e == f)
+        {
+            count += 1;
+        }
+
+        current += 1;
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        let current = 987_654;
+
+        let (a, b, c, d, e, f) = (
+            current / 100_000 % 10,
+            current / 10_000 % 10,
+            current / 1_000 % 10,
+            current / 100 % 10,
+            current / 10 % 10,
+            current % 10,
+        );
+
+        assert_eq!((a, b, c, d, e, f), (9, 8, 7, 6, 5, 4));
+    }
+
+    #[test]
+    fn test_dumb() {
+        assert_eq!(dumb(172_930, 683_082), 1675);
+    }
+}
